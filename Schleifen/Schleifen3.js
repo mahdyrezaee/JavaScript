e35c2612ef71e928de0541b5b58cead6852291e6
mahdi rezaee <rezaee012636@gmail.com>
2024-04-01 15:37:50 +0200

    Schleifen ist fertig

--- Schleifen/Schleifen3.js
+++ Schleifen/Schleifen3.js
@@ -5,11 +5,25 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function execute() {
-    let nr;
+    let nr1;
     do {
-         nr = parseInt(await prompt("Please enter your nr:  "));
-    }while (  isNaN(nr) || nr <= 1 || nr >= 100);
-    console.log(nr)
+
+        nr1 = parseInt(await prompt("Please enter your nr1:  "));
+    }while ( isNaN(nr1) || nr1 <= 1 || nr1 >= 100);
+    console.clear();
+    let grlegenheit=0;
+    let nr2;
+    do {
+        nr2 = parseInt(await prompt("Please enter your nr2:  "));
+        if (nr1 >= nr2){
+            console.log(" nr1 ist gross ");
+        }
+        if (nr1 <= nr2){
+            console.log(" nr1 ist klein ");
+        }
+        grlegenheit +=1;
+    }while ( nr2 !== nr1);
+    console.log(" alles test "+ grlegenheit);
 }
 
 execute().catch((err) => { console.error(err); }).finally(() => rl.close());
