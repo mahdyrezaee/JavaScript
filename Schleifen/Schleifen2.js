3db36c6f0ff47d07e084c6d33c96419344202cdf
mahdi rezaee <rezaee012636@gmail.com>
2024-04-09 12:03:39 +0200

    Ich habe nue Array gebaut

--- Schleifen/Schleifen2.js
+++ Schleifen/Schleifen2.js
@@ -5,10 +5,22 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
 async function execute() {
-    const serson = { vorname: "Mahdi", nachname: "Rezaee", alt: 18 ,grosse:200};
-    for (let i in serson) {
+    const person =[
+        { vorname: "Helin", nachname: "Mohammadi", alt: 39 ,grosse:140},
+    { vorname: "Mahdi", nachname: "Rezaee", alt: 18 ,grosse:200},
+    { vorname: "Nati", nachname: "Mirzaee", alt: 55 ,grosse:125},
+        { vorname: "ssad", nachname: "Qolami", alt: 9 ,grosse:100}
+]
+
+    for (let i in person) {
         console.log(i);
-        console.log(serson[i]);
+        console.log(person[i]);
+        console.log();
+    }
+    console.log("*************************************************************");
+    for (let i of person){
+        console.log();
+        console.log(i);
     }
 }
 
